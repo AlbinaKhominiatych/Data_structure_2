b70362397e9bc46c8783cbdb75c2a5235fc53a23
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-29 20:02:29 +0200

    за допомогою однозв'язного списку

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
-from queue import LifoQueue
+#за допомогою однозв'язного списку
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class Stack:
+    def __init__(self):
+        self.head = Node("head")
+        self.size = 0
+    def __str__(self):
+        cur = self.head.next
+        out = ""
+        while cur:
+            out += str(cur.value) + " -> "
+            cur = cur.next
+        return out[:-4]
 
-stack = LifoQueue(maxsize=3)
-print(stack.qsize())#кількість елементів
-stack.put("a")
-stack.put("b")
-stack.put("c")
+    def getSize(self):
+        return self.size
 
-print(stack.full())
-print(stack.qsize())#кількість елементів
-for i in range(stack.qsize()):
-    print(stack.get())
+    def isEmpty(self):
+        return self.size == 0
+
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("стек порожній, неможливо продивитися")
+        return self.head.next.value
+
+    def push(self, value):
+        node = Node(value)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("стек порожній, неможливо видалити")
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return remove.value
+
+stack = Stack()
+for i in range(1, 11):
+    stack.push(i)
+print(stack)
+print(stack.pop())
+print(stack.peek())
+print(stack)
